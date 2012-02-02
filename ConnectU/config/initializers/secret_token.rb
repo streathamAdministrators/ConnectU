04b2e2d22af1d7195d59193d1e86dcaffa3e46b5
aubindesign <aubin.lorieux@supinfo.com>
2012-02-02 15:43:44 +0000

    Installation de Rails

---
+++ ConnectU/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ConnectU::Application.config.secret_token = 'cd30aeeceeb851b93affbe510c411be3b944f07a7b44fd1eabfe7d9d77e155f3535d59f46af618f37c1414a3d635001c761e7ba7702afb37e2ac979b7bd9e6f1'
