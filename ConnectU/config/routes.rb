241489a8f80e9251891e0d343b6037a71db6e3f7
Damien PUIG <puigfb@gmail.com>
2012-02-02 16:14:19 +0000

    mise en place root :to

--- ConnectU/config/routes.rb
+++ ConnectU/config/routes.rb
@@ -2,6 +2,8 @@ ConnectU::Application.routes.draw do
   resources :users
 
   resources :information
+  
+  root :to => 'users#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
